dba97f7ffc5e52c4fb48e09ecf4e374709c29a85
Andy Lau <andyhplau@gmail.com>
2024-02-04 23:01:09 -0800

    feat: implemented home page for v1 and v2

--- V2_Safe/index.js
+++ V2_Safe/index.js
@@ -1,21 +1,29 @@
 const express = require("express");
-// Import any necessary modules or libraries here
 
-// Define your functions and variables here
-
-// Add event listeners or other initialization code here
-
-// Start executing your code here
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
-// Define your routes and middleware here
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("This is V2 Safe.");
+  res.render("index");
 });
 
-// Add more routes here
+app.get("/signup", (req, res) => {
+  res.send("signup page");
+});
+
+app.get("/login", (req, res) => {
+  res.send("login page");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", (req, res) => {
+  res.status(404);
+  res.send("Page not found - 404");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
