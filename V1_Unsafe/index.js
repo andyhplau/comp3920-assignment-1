9cb30f4b6a0bbe26b20b42fdacbf21991c1f3007
Andy Lau <andyhplau@gmail.com>
2024-02-04 21:59:15 -0800

    chore: initial commit for V1_Unsafe

---
+++ V1_Unsafe/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+// Import any necessary modules or libraries here
+
+// Define your functions and variables here
+
+// Add event listeners or other initialization code here
+
+// Start executing your code here
+const app = express();
+const port = 3000;
+
+// Define your routes and middleware here
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Add more routes here
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
