e80cd56e4eac9055e2ea35890672615cc77ca130
Andy Lau <andyhplau@gmail.com>
2024-02-05 21:55:35 -0800

    fix: fixed mongodb url

--- V1_Unsafe/index.js
+++ V1_Unsafe/index.js
@@ -30,7 +30,7 @@ const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 var mongoStore = MongoStore.create({
-  mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@assignment1-v1.o5clsqp.mongodb.net/?retryWrites=true&w=majority`,
+  mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@assignment1-v1.o5clsqp.mongodb.net/sessions`,
   crypto: {
     secret: mongodb_session_secret,
   },
