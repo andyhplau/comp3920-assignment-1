4ce5a227d7dedf7a00663f78dde8a02cedcd1a40
Andy Lau <andyhplau@gmail.com>
2024-02-05 21:30:19 -0800

    feat: implemented the unsafe website

--- V1_Unsafe/index.js
+++ V1_Unsafe/index.js
@@ -1,17 +1,88 @@
+require("./utils");
+require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const bcrypt = require("bcrypt");
 saltRounds = 12;
 
+const { printMySQLVersion } = include("database/db_utils");
+const { createUser, getUser } = include("database/users");
+printMySQLVersion();
+
 const app = express();
 const port = process.env.PORT || 3000;
-
-var users = [];
-
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+const expireTime = 60 * 60 * 1000;
+const picURL = {
+  0: "lakeLouise.jpg",
+  1: "mountain.jpg",
+  2: "sunrise.jpg",
+};
+const getRandomPicIndex = () => {
+  return Math.floor(Math.random() * 2);
+};
+
+// MongoDB setup
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+var mongoStore = MongoStore.create({
+  mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@assignment1-v1.o5clsqp.mongodb.net/?retryWrites=true&w=majority`,
+  crypto: {
+    secret: mongodb_session_secret,
+  },
+});
+
+// Session setup
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+app.use(
+  session({
+    secret: node_session_secret,
+    store: mongoStore,
+    saveUninitialized: false,
+    resave: true,
+  })
+);
+
+// Middlewares
 app.use(express.urlencoded({ extended: false }));
 
+function isValidSession(req) {
+  if (req.session.authenticated) {
+    return true;
+  }
+  return false;
+}
+
+function sessionValidation(req, res, next) {
+  if (!isValidSession(req)) {
+    req.session.destroy();
+    res.redirect("/");
+    return;
+  } else {
+    next();
+  }
+}
+
+app.use("/members", sessionValidation);
+
+// Routes
 app.get("/", (req, res) => {
-  res.render("index");
+  if (isValidSession(req)) {
+    res.render("index", {
+      username:
+        req.session.username.charAt(0).toUpperCase() +
+        req.session.username.slice(1),
+    });
+  } else {
+    req.session.destroy();
+    res.render("index", {
+      username: null,
+    });
+  }
 });
 
 app.get("/signup", (req, res) => {
@@ -23,7 +94,7 @@ app.get("/signup", (req, res) => {
   });
 });
 
-app.post("/signingUp", (req, res) => {
+app.post("/signingUp", async (req, res) => {
   var username = req.body.username;
   var password = req.body.password;
   if (!username && !password) {
@@ -34,18 +105,24 @@ app.post("/signingUp", (req, res) => {
     res.redirect("/signup?missingPassword=1");
   } else {
     var hashedPassword = bcrypt.hashSync(password, saltRounds);
-    users.push({ username: username, password: hashedPassword });
+    var success = await createUser({
+      user: username,
+      hashedPassword: hashedPassword,
+    });
 
-    console.log(users);
-
-    var usershtml = "";
-    for (i = 0; i < users.length; i++) {
-      usershtml +=
-        "<li>" + users[i].username + ": " + users[i].password + "</li>";
+    if (success) {
+      req.session.authenticated = true;
+      req.session.username = username;
+      req.session.cookie.maxAge = expireTime;
+      res.render("members", {
+        username:
+          req.session.username.charAt(0).toUpperCase() +
+          req.session.username.slice(1),
+        picURL: picURL[getRandomPicIndex()],
+      });
+    } else {
+      res.render("errorMessage", { error: "Failed to create user." });
     }
-
-    var html = "<ul>" + usershtml + "</ul>";
-    res.send(html);
   }
 });
 
@@ -62,7 +139,7 @@ app.get("/login", (req, res) => {
   });
 });
 
-app.post("/loggingIn", (req, res) => {
+app.post("/loggingIn", async (req, res) => {
   var username = req.body.username;
   var password = req.body.password;
 
@@ -73,28 +150,45 @@ app.post("/loggingIn", (req, res) => {
   } else if (!password) {
     res.redirect("/login?missingPassword=1");
   } else {
-    for (i = 0; i < users.length; i++) {
-      if (users[i].username == username) {
-        if (bcrypt.compareSync(password, users[i].password)) {
-          res.redirect("/loggedIn");
+    var results = await getUser({
+      user: username,
+      hashedPassword: password,
+    });
+    if (results) {
+      if (results.length == 1) {
+        if (bcrypt.compareSync(password, results[0].password)) {
+          req.session.authenticated = true;
+          req.session.username = username;
+          req.session.cookie.maxAge = expireTime;
+          res.redirect("/members");
         } else {
           res.redirect("/login?incorrectPassword=1");
         }
-      } else {
-        res.redirect("/login?incorrectUsername=1");
       }
+    } else {
+      res.redirect("/login?incorrectUsername=1");
     }
   }
 });
 
-app.get("/loggedIn", (req, res) => {
-  var html = `
-  You are logged in!
-  `;
-  res.send(html);
+app.get("/members", (req, res) => {
+  if (!req.session.authenticated) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("members", {
+    username:
+      req.session.username.charAt(0).toUpperCase() +
+      req.session.username.slice(1),
+    picURL: picURL[getRandomPicIndex()],
+  });
 });
 
-app.use(express.static(__dirname + "/public"));
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+});
 
 app.get("*", (req, res) => {
   res.status(404);
